43724ea4d5ef2d739e6fa685767cdc020777b80e
anweshatd <124930877+anweshatd@users.noreply.github.com>
2023-11-14 12:45:41 +0000

    Add files via upload

--- Light.hs
+++ Light.hs
@@ -1,4 +1,5 @@
 import Data.List
+import Text.Printf
 
 -- Function to check if given number is prime
 prime :: Int -> Bool
@@ -13,7 +14,23 @@ factorisable f n
 
 --calculates number of lit segements in given tuple
 numLitSeg :: (Int, Int, Int, Int) -> Int
-numLitSeg (hr, mn, dy, mt) 
+numLitSeg (hr, mn, dy, mt) = 
+    let str = concat [if n < 10 then '0' : show n else show n | n <- [hr, mn, dy, mt]] --converts tuple to a string
+    in foldl (\sum char -> sum + charToValue char) 0 str --counts and adds number of lit segments in string and stores them in sum
+
+--converts each character in the string 
+charToValue :: Char -> Int
+charToValue char = case char of
+    '0' -> 6
+    '1' -> 2
+    '2' -> 5
+    '3' -> 5
+    '4' -> 4
+    '5' -> 5
+    '6' -> 6
+    '7' -> 3
+    '8' -> 7
+    '9' -> 6
 
 
 --checks if given tuple has no duplicate numbers and the number of lit segments is prime
@@ -23,9 +40,10 @@ magic (hr, mn, dy, mt) =
     && prime (numLitSeg(hr, mn, dy, mt))
 
 --Checks if given tuple of ints has duplicates in it
-nodups :: Eq a => (a, a, a, a) -> Bool
-nodups (hr, mn, dy, mt) =
-    (hr, mn, dy, mt) == nub (hr, mn, dy, mt)
+nodups :: (Int, Int, Int, Int) -> Bool
+nodups (hr, mn, dy, mt) = 
+   let nodupsStr = concat [if n < 10 then '0' : show n else show n | n <- [hr, mn, dy, mt]] --convert tuple of ints to string
+   in length nodupsStr == length (nub nodupsStr) --compares length of original string with string after removing duplicates if any
 
 
 --generates list of tuples with hour, minutes, day and month
@@ -34,21 +52,41 @@ generator1 =
     [ (hr, mn, dy, mt)
       | hr <- [00 .. 23],
         mn <- [00 .. 59],
-        dy <- [01 .. 365],
+        dy <- [01 .. 31],
         mt <- [01 .. 12]
     ]
 
+addMin :: (Int, Int, Int, Int) -> (Int, Int, Int, Int)
+addMin(hr, mn, dy, mt) 
+    | mn == 59 = (hr + 1, 00, dy, mt)
+    | otherwise = (hr, mn + 1, dy, mt)
 
 tester1 :: (Int, Int, Int, Int) -> Bool
 tester1(hr, mn, dy, mt) =
-    magic (hr, mn, dy, mt)
-    && magic (hr + 24, mn, dy + 1, mt)
-    && numLitSeg(hr + 24, mn + 1, dy + 1, mt) == (numLitSeg(hr, mn, dy, mt)+ numLitSeg(hr + 24, mn, dy + 1, mt)) `div` 2
+    magic (hr, mn, dy, mt) --tuple is magic
+    && magic (hr, mn, dy + 1, mt) --tuple exactly one day later is magic
+    && numLitSeg(addMin(hr, mn, dy + 1, mt)) == (numLitSeg(hr, mn, dy, mt)+ numLitSeg(hr, mn, dy + 1, mt)) `div` 2
+    -- && numLitSeg(hr, mn, dy, mt) == (numLitSeg(hr, mn + 1, dy + 1, mt) + numLitSeg(hr, mn, dy + 1, mt)) `div` 2 ?
 
+
+x_tester1 :: Int 
+x_tester1 =
+    length [t | t <- ts, tester1 t] 
+    where
+    ts =
+        [ ( 6,59,17,24)
+        , ( 6,59,17,34)
+        , ( 6,59,27,14) , ( 6,59,27,41) , ( 8,59,12,46) , (16,59, 7,24) , (16,59, 7,42)
+        , (16,59, 7,43) , (16 ,59 ,27 ,40) , (18,59, 2,46) ]
+
+testmagicday :: (Int, Int, Int, Int) -> Bool
+testmagicday(hr, mn, dy, mt) =
+    numLitSeg(hr, mn + 1, dy + 1, mt) == (numLitSeg(hr, mn, dy, mt)+ numLitSeg(hr, mn, dy + 1, mt)) `div` 2
 
 main :: IO()
 main = 
     print(filter tester1 generator1)
+    --print (numLitSeg(17, 0, 28, 4))
 
 {-
 --testing
